581b55491fe7a6ff9cd8b751b6efa0660bb06b4b
Charles Smith <techdocsmith@gmail.com>
2021-12-02 12:29:35 -0800

    day 1

--- kafka/consume.py
+++ kafka/consume.py
@@ -1,25 +1,37 @@
 from kafka import KafkaConsumer
-#from prometheus_api_client import PrometheusConnect
+import sys
 
-#prom = PrometheusConnect(url ="http://localhost:9090", disable_ssl=True)
-
-#for metric in prom.all_metrics():
-#    print(metric)
-#consumer = KafkaConsumer('prometheus')
+# TODO make configs/args:
+# bootstrap server (default 'localhost:9092')
+# topic
+# no. of messages to consume
+# consume in 1 by 1 mode
 
 #topic = 'wikipedia'
-topic = 'wikistream'
+#topic = 'docs-demo'
+topic = 'mini-demo'
+
+print("initialize consumer")
+consumer = KafkaConsumer (topic, bootstrap_servers=['localhost:29092'], auto_offset_reset='earliest',
+     enable_auto_commit=True )
+print("loop through messages")
+for msg in consumer:
+    print("got a message")
+    print(msg.value)
+    print()
+    proceed = input("Type 'X' + Enter to exit. Enter to continue.").lower()
+    if proceed == 'x':
+        print("TTFN")
+        sys.exit()
+
+# SASL config
 #sasl_mechanism = "PLAIN"
 #username = "admin"
 #password = "12345"
 #security_protocol = "SASL_PLAINTEXT"
-
-
-consumer = KafkaConsumer (topic, bootstrap_servers=['localhost:29092'])#,
+# #,
 #                              api_version=(0, 10),
 #                              security_protocol=security_protocol,
 #                              sasl_mechanism = sasl_mechanism,
 #                              sasl_plain_username = username,
 #                              sasl_plain_password = password)
-for msg in consumer:
-    print (msg)
